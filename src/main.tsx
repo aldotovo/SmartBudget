341d80baa610d33ed6d69d94e1ec219994c93576
Aldo Tovo <aldo_tovo@hotmail.com>
2026-06-25 10:28:49 -0300

    Fixing

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,13 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { UserProvider } from './contexts/UserContext'
-
-// @ts-ignore - CSS module
 import './index.css'
-
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App'
+import { UserProvider } from './contexts/UserContext' // <-- IMPORTE
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <UserProvider>
+    <UserProvider>  {/* <-- ENVOLVA O APP */}
       <App />
     </UserProvider>
   </StrictMode>,
